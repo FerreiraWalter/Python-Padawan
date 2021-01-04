70246618643866b527adbb432a12b5c41dd83cf8
Walter Ferreira <FerreiraWalter@users.noreply.github.com>
2021-01-04 19:51:40 -0300

    Operadores

---
+++ Aula 2/Operadores/Operadores.py
@@ -0,0 +1,17 @@
+#Operadores Aritméticos
+
+soma = 1 + 1
+subtracao = 1 - 1
+multiplicacao = 1 * 1
+divisao = 1 / 1
+divisao_inteira = 1 // 1
+expoente = 1**1
+
+#Operadores de Comparação
+
+comparacao = 4 > 5
+#Isso vai FALSE
+outra_comparacao = 6 == 6
+#Isso vai ser TRUE
+
+#Veremos os outros em exercicios mais para frente
